69b1c68133fb3a0ca798315189eb2f5e9c268113
MrGuy987 <2030swang@student.ncc.vic.edu.au>
2026-02-22 09:59:37 +1100

    Create src/planes.js with plane models

---
+++ src/planes.js
@@ -0,0 +1,33 @@
+// Plane Models
+const planes = [
+    {
+        name: 'Cessna',
+        type: 'General Aviation',
+        speed: '140 knots',
+        capacity: 4,
+        description: 'A small, single-engine aircraft suitable for personal or training.'
+    },
+    {
+        name: 'Boeing 747',
+        type: 'Commercial Airliner',
+        speed: '570 knots',
+        capacity: 660,
+        description: 'A large aircraft used for long-haul flights.'
+    },
+    {
+        name: 'Fighter Jet',
+        type: 'Military',
+        speed: '1,200 knots',
+        capacity: 1,
+        description: 'Designed for air-to-air combat.'
+    },
+    {
+        name: 'Glider',
+        type: 'Recreational',
+        speed: '40 knots',
+        capacity: 2,
+        description: 'A lightweight aircraft that is designed to glide.'
+    }
+];
+
+module.exports = planes;
